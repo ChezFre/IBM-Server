f369987f144ff6132511def899f002a0aed4298e
Fre Vandeborne <frederic.vandeborne@icapps.com>
2019-08-22 21:45:15 +0200

    Switched to streamer implementation instead of url

--- routes/index.js
+++ routes/index.js
@@ -7,24 +7,14 @@ const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const directory = path.join(__dirname, '../public/uploads');
-    cb(null, directory)
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-  }
-})
-
-const upload = multer({ storage });
+const upload = multer({ dest: 'tmp/' });
 
 router.get('/', function(req, res, next) {
   res.status(404).json('Please upload via /upload');
 });
 
 router.post('/upload', upload.single('image'), async (req, res, next) => {
-  const file = req.file;
+  const { file } = req;
   const baseUrl = req.protocol + "://" + req.headers.host;
   const publicPath = '/uploads/' + file.filename;
 
@@ -33,19 +23,17 @@ router.post('/upload', upload.single('image'), async (req, res, next) => {
     iam_apikey: process.env.IBM_API_KEY
   });
 
-  visualRecognition.classify({
-    url: baseUrl + publicPath,
+  const classification = await visualRecognition.classify({
+    images_file: fs.createReadStream(req.file.path),
     classifier_ids: ['food']
-  }, async (err, response) => {
-      if (err) res.sendStatus(500);
-  
-      await fs.unlink(file.path, (err) => {
-      if (err) throw err;
-        console.log(`${file.path} was deleted`);
-        
-      return res.json(response);
-    });
-  })
+  });
+
+  await fs.unlink(file.path, (err) => {
+    if (err) throw err;
+    console.log(`${file.path} was deleted`);
+  });
+
+  res.json(classification);
 });
 
 module.exports = router;
